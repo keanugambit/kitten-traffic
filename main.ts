7340d054acb2abfee54d04c6a49ebbb046741b2a
keanugambit <67330798+keanugambit@users.noreply.github.com>
2020-11-25 07:26:23 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,29 @@
-let mySprite = sprites.create(img`
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        4 4 4 . . . . 4 4 4 . . . . . . 
+        4 3 4 4 4 4 4 4 3 4 . . . . . . 
+        4 3 4 7 4 4 7 4 3 4 . . . . . . 
+        4 4 4 7 4 4 7 4 4 e . . . . . . 
+        4 4 4 4 4 4 4 4 4 e 4 4 e 4 . . 
+        4 4 4 4 3 4 4 4 4 e 4 4 e 4 4 . 
+        4 4 d d f d d 4 4 e 4 4 e 4 4 e 
+        4 4 4 f f f 4 4 4 e 4 4 e 4 4 e 
+        . 4 4 4 4 4 4 4 4 e 4 4 e 4 4 e 
+        . . 4 4 4 4 4 4 4 e 4 4 . . 4 4 
+        . . . . e e . . . . e e . . e e 
+        . . . . 4 4 . . . . 4 4 . . 4 4 
+        . . . . 4 4 . . . . 4 4 . . 4 4 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(right)
+})
+let right: Image = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     4 4 4 . . . . 4 4 4 . . . . . . 
@@ -16,6 +41,9 @@ let mySprite = sprites.create(img`
     . . . . 4 4 . . . . 4 4 . . 4 4 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+let left = mySprite.image
+mySprite.image.flipX()
+right = mySprite.image
 tiles.setTilemap(tilemap`level1`)
 controller.player1.moveSprite(mySprite)
 for (let value of tiles.getTilesByType(sprites.dungeon.floorLight2)) {
