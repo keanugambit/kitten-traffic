67aaca0cb9ab79d8c43998ac3910444d7060363a
keanugambit <67330798+keanugambit@users.noreply.github.com>
2020-11-25 07:18:00 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . 4 f f 4 . . . . . . 
-    . . . . . 4 4 4 4 4 4 . . . . . 
-    . . . . 4 4 7 4 4 7 4 4 . . . . 
-    . . . 4 3 4 7 4 4 7 4 3 4 . . . 
-    . . . 4 3 f f f f f f 3 4 . . . 
-    . . . 4 4 f 4 4 4 4 f 4 4 . . . 
-    . . . . . 4 4 4 4 4 4 . . . . . 
-    . . . . . 4 4 4 4 4 4 . . . . . 
-    . . . . . 4 4 4 4 4 4 . . . . . 
-    . . . . . 4 4 4 4 4 4 . . . . . 
-    . . . . . 4 4 . . 4 4 . . . . . 
-    . . . . . 4 4 . . 4 4 . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    4 4 4 . . . . 4 4 4 . . . . . . 
+    4 3 4 4 4 4 4 4 3 4 . . . . . . 
+    4 3 4 7 4 4 7 4 3 4 . . . . . . 
+    4 4 4 7 4 4 7 4 4 e . . . . . . 
+    4 4 4 4 4 4 4 4 4 e 4 4 e 4 . . 
+    4 4 4 4 3 4 4 4 4 e 4 4 e 4 4 . 
+    4 4 d d f d d 4 4 e 4 4 e 4 4 e 
+    4 4 4 f f f 4 4 4 e 4 4 e 4 4 e 
+    . 4 4 4 4 4 4 4 4 e 4 4 e 4 4 e 
+    . . 4 4 4 4 4 4 4 e 4 4 . . 4 4 
+    . . . . e e . . . . e e . . e e 
+    . . . . 4 4 . . . . 4 4 . . 4 4 
+    . . . . 4 4 . . . . 4 4 . . 4 4 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 tiles.setTilemap(tilemap`level1`)
-controller.player2.moveSprite(mySprite)
+controller.player1.moveSprite(mySprite)
 for (let value of tiles.getTilesByType(sprites.dungeon.floorLight2)) {
     tiles.setWallAt(value, true)
 }
+tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 8))
+scene.cameraFollowSprite(mySprite)
