17e3fa79b2d4caff8ba86e8873be660390991d12
keanugambit <67330798+keanugambit@users.noreply.github.com>
2020-11-25 06:56:39 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,36 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+........................
+..444....444............
+..4344444434............
+..4347447434............
+..444744744e............
+..444444444e44e44e44e4..
+..444434444e44e44e44e44.
+..44ddfdd44e44e44e44e44.
+..444fff444e44e44e44e44.
+...44444444e44e44e44e44.
+....4444444e44.44....44.
+......ee....ee.ee....ee.
+......44....44.44....44.
+......44....44.44....44.
+......44................
+........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
